257023f0bcb080ca0be9b1e2b9df00a5b433af47
Faisal <shahfaisal1234@gmail.com>
2026-06-27 12:55:30 +0500

    Setup.py file updated

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
-"""
-setup.py
-----------
-Yeh file project ko ek INSTALLABLE PYTHON PACKAGE banati hai
-(taake "from customer_intelligence_suite.utils import ..." jaise
-imports kaam karein, project ko kahin se bhi).
+from setuptools import setup, find_packages
 
-Standard setup.py (aap ke template jaisa hi):
-
-    from setuptools import find_packages, setup
-
-    setup(
-        name="customer_intelligence_suite",
-        version="0.0.1",
-        author="Faisal",
-        packages=find_packages(),
-        install_requires=[],   # requirements.txt se separately install karenge
-    )
-
-Phir terminal mein: pip install -e .
-"""
+setup(
+    name="customer_intelligence_suite",
+    version="0.0.0",
+    author="Faisal Mahmood",
+    author_email="shahfaisal1234@gmail.com",
+    packages=find_packages()
+)
